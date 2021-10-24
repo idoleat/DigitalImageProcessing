676b2599aed7145c8b1e1954c80c3b0328a5eb9b
idoleat <dppss92132@gmail.com>
2021-10-24 08:47:40 +0000

    Add a test funciton to make R in (r,g,b) all 255
    Test the example from official document:
    https://docs.microsoft.com/zh-tw/dotnet/api/system.drawing.imaging.bitmapdata?view=windowsdesktop-5.0
    It works.

--- DIP/Form1.Designer.cs
+++ DIP/Form1.Designer.cs
@@ -31,12 +31,18 @@ namespace DIP
         {
             this.btn_OpenImage = new System.Windows.Forms.Button();
             this.btn_SaveImage = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.SourceImage = new System.Windows.Forms.PictureBox();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.ProcessedImage = new System.Windows.Forms.PictureBox();
+            this.RGB = new System.Windows.Forms.Label();
+            this.RGB_r = new System.Windows.Forms.Button();
+            this.RGB_g = new System.Windows.Forms.Button();
+            this.RGB_b = new System.Windows.Forms.Button();
+            this.RGB_grey = new System.Windows.Forms.Button();
+            this.test = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.SourceImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProcessedImage)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_OpenImage
@@ -59,41 +65,107 @@ namespace DIP
             this.btn_SaveImage.UseVisualStyleBackColor = true;
             this.btn_SaveImage.Click += new System.EventHandler(this.onClick_SaveImage);
             // 
-            // pictureBox1
+            // SourceImage
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(253, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(378, 244);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.SourceImage.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.SourceImage.Enabled = false;
+            this.SourceImage.Location = new System.Drawing.Point(253, 12);
+            this.SourceImage.Name = "SourceImage";
+            this.SourceImage.Size = new System.Drawing.Size(378, 244);
+            this.SourceImage.TabIndex = 2;
+            this.SourceImage.TabStop = false;
             // 
             // openFileDialog
             // 
             this.openFileDialog.FileName = "openFileDialog";
             // 
-            // pictureBox2
+            // ProcessedImage
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(253, 274);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(378, 244);
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
+            this.ProcessedImage.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ProcessedImage.Location = new System.Drawing.Point(253, 274);
+            this.ProcessedImage.Name = "ProcessedImage";
+            this.ProcessedImage.Size = new System.Drawing.Size(378, 244);
+            this.ProcessedImage.TabIndex = 3;
+            this.ProcessedImage.TabStop = false;
+            // 
+            // RGB
+            // 
+            this.RGB.AutoSize = true;
+            this.RGB.Location = new System.Drawing.Point(12, 56);
+            this.RGB.Name = "RGB";
+            this.RGB.Size = new System.Drawing.Size(152, 12);
+            this.RGB.TabIndex = 4;
+            this.RGB.Text = "RGB Seperation and Grey scale";
+            // 
+            // RGB_r
+            // 
+            this.RGB_r.Location = new System.Drawing.Point(10, 83);
+            this.RGB_r.Name = "RGB_r";
+            this.RGB_r.Size = new System.Drawing.Size(35, 33);
+            this.RGB_r.TabIndex = 5;
+            this.RGB_r.Text = "R";
+            this.RGB_r.UseVisualStyleBackColor = true;
+            this.RGB_r.Click += new System.EventHandler(this.RGB_r_Click);
+            // 
+            // RGB_g
+            // 
+            this.RGB_g.Location = new System.Drawing.Point(51, 83);
+            this.RGB_g.Name = "RGB_g";
+            this.RGB_g.Size = new System.Drawing.Size(35, 33);
+            this.RGB_g.TabIndex = 6;
+            this.RGB_g.Text = "G";
+            this.RGB_g.UseVisualStyleBackColor = true;
+            // 
+            // RGB_b
+            // 
+            this.RGB_b.Location = new System.Drawing.Point(92, 83);
+            this.RGB_b.Name = "RGB_b";
+            this.RGB_b.Size = new System.Drawing.Size(35, 33);
+            this.RGB_b.TabIndex = 7;
+            this.RGB_b.Text = "B";
+            this.RGB_b.UseVisualStyleBackColor = true;
+            // 
+            // RGB_grey
+            // 
+            this.RGB_grey.Location = new System.Drawing.Point(133, 83);
+            this.RGB_grey.Name = "RGB_grey";
+            this.RGB_grey.Size = new System.Drawing.Size(96, 33);
+            this.RGB_grey.TabIndex = 8;
+            this.RGB_grey.Text = "Grey";
+            this.RGB_grey.UseVisualStyleBackColor = true;
+            // 
+            // test
+            // 
+            this.test.Location = new System.Drawing.Point(696, 20);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(76, 26);
+            this.test.TabIndex = 9;
+            this.test.Text = "test";
+            this.test.UseVisualStyleBackColor = true;
+            this.test.Click += new System.EventHandler(this.test_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(940, 539);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.RGB_grey);
+            this.Controls.Add(this.RGB_b);
+            this.Controls.Add(this.RGB_g);
+            this.Controls.Add(this.RGB_r);
+            this.Controls.Add(this.RGB);
+            this.Controls.Add(this.ProcessedImage);
+            this.Controls.Add(this.SourceImage);
             this.Controls.Add(this.btn_SaveImage);
             this.Controls.Add(this.btn_OpenImage);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SourceImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProcessedImage)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -101,10 +173,16 @@ namespace DIP
 
         private System.Windows.Forms.Button btn_OpenImage;
         private System.Windows.Forms.Button btn_SaveImage;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox SourceImage;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox ProcessedImage;
+        private System.Windows.Forms.Label RGB;
+        private System.Windows.Forms.Button RGB_r;
+        private System.Windows.Forms.Button RGB_g;
+        private System.Windows.Forms.Button RGB_b;
+        private System.Windows.Forms.Button RGB_grey;
+        private System.Windows.Forms.Button test;
     }
 }
 
--- DIP/Form1.cs
+++ DIP/Form1.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace DIP
 {
     public partial class Form1 : Form
     {
+        public Bitmap SourceBitmap;
         public Form1()
         {
             InitializeComponent();
@@ -20,17 +23,56 @@ namespace DIP
         private void onClick_OpenImage(object sender, EventArgs e)
         {
             openFileDialog.Filter = "All Files|*.*|Bitmap File|*.bmp|Jpeg Files|*.jpg";
-            if (openFileDialog.ShowDialog() == DialogResult.OK) pictureBox1.Image = new Bitmap(openFileDialog.FileName);
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                SourceBitmap = new Bitmap(openFileDialog.FileName);
+                SourceImage.Image = SourceBitmap;
+            }
         }
 
         private void onClick_SaveImage(object sender, EventArgs e)
         {
+            
+
 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void RGB_r_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void test_Click(object sender, EventArgs e)
+        {
+            Rectangle rect = new Rectangle(0, 0, SourceBitmap.Width, SourceBitmap.Height);
+            BitmapData bmpData = SourceBitmap.LockBits(rect, ImageLockMode.ReadWrite, SourceBitmap.PixelFormat);
+
+            // Get the address of the first line.
+
+            // Declare an array to hold the bytes of the bitmap.
+            int bytes = Math.Abs(bmpData.Stride) * SourceBitmap.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            // Copy the RGB values into the array.
+            Marshal.Copy(bmpData.Scan0, rgbValues, 0, bytes);
+
+            // Set every third value to 255. A 24bpp bitmap will look red.  
+            for (int counter = 2; counter < rgbValues.Length; counter += 3)
+                rgbValues[counter] = 255;
+
+            // Copy the RGB values back to the bitmap
+            Marshal.Copy(rgbValues, 0, bmpData.Scan0, bytes);
+
+            // Unlock the bits.
+            SourceBitmap.UnlockBits(bmpData);
+
+            // Draw the modified image.
+            ProcessedImage.Image = SourceBitmap;
         }
     }
 }
