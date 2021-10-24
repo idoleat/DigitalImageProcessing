3e0d8f5710cd7bdb93b0aae717f976cfb0dfda81
idoleat <dppss92132@gmail.com>
2021-10-24 13:55:53 +0000

    Add basic functionality of process and undo
    undo is achieved by stored processed data in a list. Pop list last element when undo. Result image always shows the last element of the list.
    However it's not functional temporarity.

--- DIP/Form1.cs
+++ DIP/Form1.cs
@@ -14,7 +14,11 @@ namespace DIP
 {
     public partial class Form1 : Form
     {
-        public Bitmap SourceBitmap;
+        public Bitmap OpenedBitmap;
+        public Bitmap ProcessedBitmap;
+        public List<BitmapData> history = new List<BitmapData>();
+        int count = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,16 +29,29 @@ namespace DIP
             openFileDialog.Filter = "All Files|*.*|Bitmap File|*.bmp|Jpeg Files|*.jpg";
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
-                SourceBitmap = new Bitmap(openFileDialog.FileName);
-                SourceImage.Image = SourceBitmap;
+                OpenedBitmap = new Bitmap(openFileDialog.FileName);
+                ProcessedBitmap = new Bitmap(openFileDialog.FileName);
+                BitmapData bmpd = ProcessedBitmap.LockBits(
+                        new Rectangle(0, 0, ProcessedBitmap.Width,
+                        ProcessedBitmap.Height),
+                        ImageLockMode.ReadWrite,
+                        ProcessedBitmap.PixelFormat
+                );
+                history.Add(bmpd);
+                ProcessedBitmap.UnlockBits(bmpd);
+                SourceImageBox.Image = OpenedBitmap;
+                ProcessedImageBox.Image = ProcessedBitmap;
             }
         }
 
         private void onClick_SaveImage(object sender, EventArgs e)
         {
-            
+            saveFileDialog.Filter = "All Files|*.*|Bitmap File|*.bmp|Jpeg Files|*.jpg";
 
-
+            if(saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                OpenedBitmap.Save(saveFileDialog.FileName);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -49,30 +66,92 @@ namespace DIP
 
         private void test_Click(object sender, EventArgs e)
         {
-            Rectangle rect = new Rectangle(0, 0, SourceBitmap.Width, SourceBitmap.Height);
-            BitmapData bmpData = SourceBitmap.LockBits(rect, ImageLockMode.ReadWrite, SourceBitmap.PixelFormat);
-
-            // Get the address of the first line.
+            BitmapData bmpData = CopyLastBitmapData();
+            //BitmapData bmpData = new BitmapData();
+            //bmpData = history.Last();
 
             // Declare an array to hold the bytes of the bitmap.
-            int bytes = Math.Abs(bmpData.Stride) * SourceBitmap.Height;
+            int bytes = Math.Abs(bmpData.Stride) * OpenedBitmap.Height;
             byte[] rgbValues = new byte[bytes];
 
             // Copy the RGB values into the array.
             Marshal.Copy(bmpData.Scan0, rgbValues, 0, bytes);
 
             // Set every third value to 255. A 24bpp bitmap will look red.  
-            for (int counter = 2; counter < rgbValues.Length; counter += 3)
-                rgbValues[counter] = 255;
+            count += 1;
+            if (count % 3 == 0)
+            {
+                for (int counter = 0; counter < rgbValues.Length; counter += 3)
+                    rgbValues[counter] = 255;
+                for (int counter = 1; counter < rgbValues.Length; counter += 3)
+                    rgbValues[counter] = 0;
+                for (int counter = 2; counter < rgbValues.Length; counter += 3)
+                    rgbValues[counter] = 0;
+            }
+            else if(count % 3 == 1)
+            {
+                for (int counter = 0; counter < rgbValues.Length; counter += 3)
+                    rgbValues[counter] = 0;
+                for (int counter = 1; counter < rgbValues.Length; counter += 3)
+                    rgbValues[counter] = 255;
+                for (int counter = 2; counter < rgbValues.Length; counter += 3)
+                    rgbValues[counter] = 0;
+            }
+            else
+            {
+                for (int counter = 0; counter < rgbValues.Length; counter += 3)
+                    rgbValues[counter] = 0;
+                for (int counter = 1; counter < rgbValues.Length; counter += 3)
+                    rgbValues[counter] = 0;
+                for (int counter = 2; counter < rgbValues.Length; counter += 3)
+                    rgbValues[counter] = 255;
+            }
+            
 
             // Copy the RGB values back to the bitmap
             Marshal.Copy(rgbValues, 0, bmpData.Scan0, bytes);
 
-            // Unlock the bits.
-            SourceBitmap.UnlockBits(bmpData);
+            history.Add(bmpData);
+            ShowHistoryLast();
+        }
+
+        private BitmapData CopyLastBitmapData()
+        {
+            BitmapData bmpData = new BitmapData();
+            bmpData.Scan0 = history.Last().Scan0;
+            bmpData.Height = history.Last().Height;
+            bmpData.Width = history.Last().Width;
+            bmpData.PixelFormat = history.Last().PixelFormat;
+            bmpData.Reserved = history.Last().Reserved;
+            bmpData.Stride = history.Last().Stride;
+
+            return bmpData;
+        }
+
+        private void ShowHistoryLast()
+        {
+            BitmapData bmpd;
+            ProcessedBitmap.LockBits(
+                        new Rectangle(0, 0, ProcessedBitmap.Width,
+                        ProcessedBitmap.Height),
+                        ImageLockMode.ReadWrite,
+                        ProcessedBitmap.PixelFormat
+            );
+            bmpd = CopyLastBitmapData();
+            //bmpd = history.Last();
+            ProcessedBitmap.UnlockBits(bmpd);
 
             // Draw the modified image.
-            ProcessedImage.Image = SourceBitmap;
+            ProcessedImageBox.Image = ProcessedBitmap;
+            // Actually before applying filters, image should be freed. But it's C#...let GC do it for you
+        }
+
+        private void btn_undo_Click(object sender, EventArgs e)
+        {
+            if (history.Count <= 1) return;
+            history.RemoveAt(history.Count - 1);
+            Console.WriteLine(history.Count);
+            ShowHistoryLast();
         }
     }
 }
