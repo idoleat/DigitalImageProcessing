a6d4b19acdad32ab29d051a66c21341dc60914b0
idoleat <dppss92132@gmail.com>
2021-10-31 06:27:44 +0000

    Add Histongram qualizer and thresholder (add files)
    WinFrom Chart is soooo wierd....I'll implement it later. Thresholder done.

--- DIP/Thres.cs
+++ DIP/Thres.cs
@@ -8,5 +8,24 @@ namespace DIP
 {
     public partial class Form1
     {
+        private void btn_Threshold_Click(object sender, EventArgs e)
+        {
+            GeneralFilter(Filter.Thres);
+        }
+
+        private void ThresholdFilter(ref byte[] rgbValue)
+        {
+            for(int i=0; i<rgbValue.Length; i++)
+            {
+                if(rgbValue[i] >= ThresVal.Value)
+                {
+                    rgbValue[i] = 255;
+                }
+                else
+                {
+                    rgbValue[i] = 0;
+                }
+            }
+        }
     }
 }
